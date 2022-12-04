4094ab2844e3a01e30a568d73ab61fa8b98bc466
MoshPe <92862841+MoshePeret@users.noreply.github.com>
2022-12-04 09:01:03 +0200

    Delete templates directory

--- templates/node.js-express/src/api/router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-
-router.post('/',(req, res) => {
-    res.status(200).send('This is your first response!\n')
-})
-
-module.exports = router
